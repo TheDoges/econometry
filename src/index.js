4ac918d3c17ed1d871b73e4cdeb64c76e1884f7f
MrBlue2583 <MrBlue2583@gmail.com>
2018-06-11 19:55:21 +0200

    add example data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const modelSolver = require ('./modelSolver');
 const fileReader = require('./fileReader');
 
+/* Example inputData in file */
+// [7.6, 156, 3],
+// [10, 114, 2.5],
+// [12.6, 115, 3.1],
+// [21.3, 67, 6],
+// [6.9, 148, 2.7]
+
 module.exports = {
     calculate: () => {
         console.table(modelSolver.solve(fileReader.getInputData()));
